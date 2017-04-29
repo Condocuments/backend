0ec822b0f2128cf1eb523bc50e0a5e36885bf24c
Rolando <rcruz@jazwings.com>
2017-04-29 16:53:36 -0400

    remove ssn

--- people/models.py
+++ people/models.py
@@ -8,7 +8,7 @@ def validate_ssn(text):
 
 
 class User(AbstractUser):
-    ssn = models.CharField(_('SSN'), max_length=15, blank=True, unique=True, validators=[validate_ssn, ])
+    ssn = models.CharField(_('SSN'), max_length=15, blank=True, unique=True, validators=[validate_ssn, ], null=True)
     license = models.CharField(_('ID or License'), max_length=20, blank=True, unique=True)
     birth_date = models.DateField(null=True, blank=True)
     profile_picture = models.ImageField(verbose_name=_('Profile Picture'), upload_to='Profile', blank=True, null=True)
