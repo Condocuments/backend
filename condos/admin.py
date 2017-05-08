c86a6e4d2d152ade0ec53a4e95e61b33a4b710dc
Rolando <rcruz@jazwings.com>
2017-05-08 12:04:58 -0400

    added admin functionality

--- condos/admin.py
+++ condos/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from condos.models import Condo, Address, Application, BedroomQuantity, Unit
+from contents.models import Content
 
 
 # Register your models here.
@@ -16,11 +17,16 @@ class BedroomQuantityAdmin(admin.StackedInline):
     extra = 3
 
 
+class ContentAdmin(admin.StackedInline):
+    model = Content
+    extra = 1
+
+
 class CondoAdmin(admin.ModelAdmin):
     list_filter = ('name', 'address')
     list_display = ('name', 'address')
     prepopulated_fields = {'slug': ('name',)}
-    inlines = [BedroomQuantityAdmin, ApplicationAdmin]
+    inlines = [ContentAdmin, BedroomQuantityAdmin, ApplicationAdmin]
 
 
 class UnitAdmin(admin.ModelAdmin):
